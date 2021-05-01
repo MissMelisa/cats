f72ec73cfc6c8a91fa96d57c375c14489d641f54
MissMelisa <melissarios497@gmail.com>
2021-05-01 20:55:51 -0300

    connect app to the API and create home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { QueryClientProvider, QueryClient } from "react-query";
+
+import "./App.css";
+import Home from "../src/Pages/Home";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
     </div>
   );
 }
