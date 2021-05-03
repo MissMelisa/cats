4cf96b9711bdabf5a955111b65c95daf170f2619
MissMelisa <melissarios497@gmail.com>
2021-05-03 11:11:06 -0300

    fix styles and add PropTypes

--- src/Components/CatPresentation/index.jsx
+++ src/Components/CatPresentation/index.jsx
@@ -1,6 +1,4 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from "@emotion/react";
+import PropTypes from "prop-types";
 
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -10,17 +8,25 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  cardMedia: { height: "140px" },
+  card: { maxWidth: "345px", height: "100%" },
+  typography: { height: "100%" },
+});
 function CatPresentation({ image, name, description }) {
+  const classes = useStyles();
   return (
-    <Card css={{ maxWidth: "345px", height: "100%" }}>
+    <Card className={classes.card}>
       <CardActionArea>
-        <CardMedia css={{ height: "140px" }} image={image} />
+        <CardMedia className={classes.cardMedia} image={image} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {name}
           </Typography>
           <Typography
-            css={{ height: "100%" }}
+            className={classes.typography}
             variant="body2"
             color="textSecondary"
             component="p"
@@ -37,5 +43,10 @@ function CatPresentation({ image, name, description }) {
     </Card>
   );
 }
+CatPresentation.propTypes = {
+  image: PropTypes.string,
+  description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default CatPresentation;
--- src/Pages/CatPresentation/index.jsx
+++ src/Pages/CatPresentation/index.jsx
@@ -1,30 +1,33 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from "@emotion/react";
 import { useQuery } from "react-query";
 
 import CatPresentation from "../../Components/CatPresentation";
 import fetchData from "../../Utils/fetchData";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  containerCats: {
+    width: "100%",
+    justifyContent: "center",
+    maxWidth: "1000px",
+
+    gap: "3em",
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+    gridTemplateRows: "auto",
+  },
+});
+
 function CatPresentationPage() {
+  const classes = useStyles();
   const { isLoading, error, data } = useQuery("repodata", () =>
     fetchData("breeds")
   );
   if (isLoading) return "Loading...";
   if (error) return "An error has ocurred: " + error.message;
-  return (
-    <div
-      css={{
-        width: "100%",
-        justifyContent: "center",
-        maxWidth: "1000px",
 
-        gap: "3em",
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-        gridTemplateRows: "auto",
-      }}
-    >
+  return (
+    <div className={classes.containerCats}>
       {data.map((item) => (
         <CatPresentation
           description={item.description}
